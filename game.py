e20725e620dc63392e296996e8289dcf6f16f95f
JeroAle <jaleman8154@floridapoly.edu>
2025-11-12 23:34:32 +0000

    Person 2 task

--- game.py
+++ game.py
@@ -1,1 +1,10 @@
+import random
+
+def rock_paper_scissor():
+    return random.choice(["rock", "paper", "scissors"])
+
+choice = rock_paper_scissor()
+
 print("Welcome to Rock-Paper-Scissors!")
+
+print(choice)
